d150db790e6a9605ca3eda71e4aae1ea87b6f460
Diana Andriasyan <86868009+dianaandriasian@users.noreply.github.com>
2022-01-09 21:57:33 +0400

    Add files via upload

---
+++ multiples3or5.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int multiplesOf3or5()
+{
+	// I use all of these variables to avoid magic numbers
+
+	// arithmetic progression of sequence 3, 6, 9 ... 996, 999
+
+	const short dFor3 = 3;
+	const short a1For3 = 3;
+	const short anFor3 = 999;
+	const short membershipFor3 = 333;
+	const int sumOfProgressionFor3 = (a1For3 + anFor3) * membershipFor3 / 2;
+
+
+	//arithmetic progression of sequence 5, 10, 15 ... 990, 995
+
+	const short dFor5 = 5;
+	const short a1For5 = 5;
+	const short anFor5 = 995;
+	const short membershipFor5 = 199;
+	const int sumOfProgressionFor5 = (a1For5 + anFor5) * membershipFor5 / 2;
+
+
+	//arithmetic progression of sequence 15, 30, 45 ... 975, 990
+
+	const short dFor15 = 15;
+	const short a1For15 = 15;
+	const short anFor15 = 990;
+	const short membershipFor15 = 66;
+	const int sumOfProgressionFor15 = (a1For15 + anFor15) * membershipFor15 / 2;
+
+	return sumOfProgressionFor3 + sumOfProgressionFor5 - sumOfProgressionFor15;
+}	
+
+int main()
+{
+	std::cout << "Multiples of 3 or 5 bellow 1000 : " << multiplesOf3or5() << std::endl;
+
+	// We can do the same as follows
+
+	int sum = 0;
+
+	sum = ((3 + 999) * 333 / 2) + ((5 + 995) * 199 / 2) - ((15 + 990) * 66 / 2);
+
+	std::cout << "Multiples of 3 or 5 bellow 1000 : " << sum << std::endl;
+
+	return 0;
+}
