8439dd4a699f94cd689ba06a49202e5580c942f7
Diana Andriasyan <86868009+dianaandriasian@users.noreply.github.com>
2022-01-09 22:20:47 +0400

    Add files via upload

---
+++ evenFibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int fibonacci(int n)
+{
+	if(n <= 1)
+		return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+	unsigned long int sumOfEvenFibonacci = 0;
+
+	// Even numbeers in the Fibonacci sequence are numbers with an index multiple of three, so...
+
+	for ( int i = 3; fibonacci(i) <= 4000000; i += 3 )
+	{
+		sumOfEvenFibonacci += fibonacci(i);
+	}
+
+	std::cout << "Sum of even fibonacci numbers that do  not exceed 4 000 000 : " <<  sumOfEvenFibonacci << std::endl;
+
+	return 0;
+}
+
